6ad1a083f042f19fb4555a423e7c8c4980426fd3
qq <666kakryatskii666@gmail.com>
2025-05-20 10:53:31 +0300

    Initial commit

---
+++ src/services/store.ts
@@ -0,0 +1,35 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+import userReducer from './slices/user-slice';
+import feedReducer from './slices/feed-slice';
+import ingredientsReducer from './slices/ingredients-slice';
+import constructorReducer from './slices/constructor-slice';
+import orderReducer from './slices/order-slice';
+
+import {
+  TypedUseSelectorHook,
+  useDispatch as dispatchHook,
+  useSelector as selectorHook
+} from 'react-redux';
+
+export const rootReducer = combineReducers({
+  user: userReducer,
+  feed: feedReducer,
+  order: orderReducer,
+  ingredients: ingredientsReducer,
+  constructorbg: constructorReducer
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production'
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch: () => AppDispatch = () => dispatchHook();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
+export default store;
